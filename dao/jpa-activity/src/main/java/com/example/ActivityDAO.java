a38a885b4c2e7dbc30e5edbcfac8ca9eed5f8118
jaydeep apte <jaydeepresources@gmail.com>
2024-11-12 16:44:28 +0530

    Add files via upload

--- dao/jpa-activity/src/main/java/com/example/ActivityDAO.java
+++ dao/jpa-activity/src/main/java/com/example/ActivityDAO.java
@@ -1,7 +1,13 @@
 package com.example;
 
+import java.util.List;
+
 public interface ActivityDAO {
 
     Activity insert(Activity activity);
+    public Activity update(Activity activity);
+    public void deleteById(Long id);
+    public Activity findById(Long id);
+    public List<Activity> findAll();
 
 }
--- dao/jpa-activity/src/main/java/com/example/ActivityDAOImpl.java
+++ dao/jpa-activity/src/main/java/com/example/ActivityDAOImpl.java
@@ -1,10 +1,7 @@
 package com.example;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
-import java.util.Scanner;
+import java.util.List;
+import javax.persistence.*;
 
 public class ActivityDAOImpl implements ActivityDAO {
     private static final EntityManagerFactory ENTITY_MANAGER_FACTORY = Persistence.createEntityManagerFactory("activity-app");
@@ -17,6 +14,62 @@ public class ActivityDAOImpl implements ActivityDAO {
 
         entityManager.persist(activity);
         entityTransaction.commit();
+        entityManager.close();
         return activity;
+    }
+
+    @Override
+    public Activity findById(Long id) {
+        EntityManager entityManager = ENTITY_MANAGER_FACTORY.createEntityManager();
+        Activity activity = null;
+        try {
+            activity = entityManager.find(Activity.class, id);
+        } finally {
+            entityManager.close();
+        }
+        return activity;
+    }
+
+    @Override
+    public List<Activity> findAll() {
+        EntityManager entityManager = ENTITY_MANAGER_FACTORY.createEntityManager();
+        List<Activity> activities = null;
+        try {
+            activities = entityManager.createQuery("SELECT a FROM Activity a", Activity.class).getResultList();
+        } finally {
+            entityManager.close();
+        }
+        return activities;
+    }
+
+    @Override
+    public Activity update(Activity activity) {
+        EntityManager entityManager = ENTITY_MANAGER_FACTORY.createEntityManager();
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        Activity updatedActivity = null;
+        try {
+            entityTransaction.begin();
+            updatedActivity = entityManager.merge(activity);
+            entityTransaction.commit();
+        } finally {
+            entityManager.close();
+        }
+        return updatedActivity;
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        EntityManager entityManager = ENTITY_MANAGER_FACTORY.createEntityManager();
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        try {
+            entityTransaction.begin();
+            Activity activity = entityManager.find(Activity.class, id);
+            if (activity != null) {
+                entityManager.remove(activity);
+            }
+            entityTransaction.commit();
+        } finally {
+            entityManager.close();
+        }
     }
 }
